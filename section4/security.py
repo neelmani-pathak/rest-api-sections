6aa6adbb7b01c761f43cde2dc0d784108bcbf214
neelmani-pathak <64878562+neelmani-pathak@users.noreply.github.com>
2020-05-10 03:02:14 -0500

    Update security.py

--- section4/security.py
+++ section4/security.py
@@ -9,10 +9,12 @@ users = [
 username_table = {u.username: u for u in users}
 userid_table = {u.id: u for u in users}
 
+
 def authenticate(username, password):
     user = username_table.get(username, None)
     if user and safe_str_cmp(user.password, password):
         return user
+
 
 def identity(payload):
     user_id = payload['identity']
